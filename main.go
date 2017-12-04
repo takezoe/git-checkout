5bad5607ccdccdae45e03146beedb47d55398646
Naoki Takezoe <takezoe@gmail.com>
2017-12-04 11:52:29 +0900

    stash and pop automatically if working tree is dirty

--- main.go
+++ main.go
@@ -29,14 +29,49 @@ func main() {
 	in := prompt.Input("branch: ", completer,
 		prompt.OptionTitle("git checkout"),
 		prompt.OptionPrefixTextColor(prompt.Blue))
-
 	r := regexp.MustCompile(`remotes/(.*)/(.*)`)
 	result := r.FindAllStringSubmatch(in, -1)
+
 	if len(result) == 0 {
-		out, _ := exec.Command("git", "checkout", in).Output()
-		fmt.Println(string(out))
+		checkout(in, func() {
+			out, _ := exec.Command("git", "checkout", in).Output()
+			fmt.Println(string(out))
+		})
 	} else {
-		out, _ := exec.Command("git", "checkout", "-b", result[0][2], result[0][1]+"/"+result[0][2]).Output()
-		fmt.Println(string(out))
+		checkout(result[0][2], func() {
+			out, _ := exec.Command("git", "checkout", "-b", result[0][2], result[0][1]+"/"+result[0][2]).Output()
+			fmt.Println(string(out))
+		})
 	}
+}
+
+func checkout(branch string, fn func()) {
+	if branch != currentBranch() {
+		isDirty := isWorkingTreeDirty()
+		if !isDirty {
+			exec.Command("git", "stash", "save")
+		}
+		fn()
+		if !isDirty {
+			exec.Command("git", "stash", "pop")
+		}
+	}
+}
+
+func currentBranch() string {
+	out, _ := exec.Command("git", "branch").Output()
+	lines := strings.Split(string(out), "\n")
+	var s string = ""
+	for i := 0; i < len(lines); i++ {
+		line := strings.TrimSpace(lines[i])
+		if strings.Index(line, "*") == 0 {
+			s = line[2:len(line)]
+		}
+	}
+	return s
+}
+
+func isWorkingTreeDirty() bool {
+	out, _ := exec.Command("git", "status").Output()
+	return strings.Index(string(out), "nothing to commit, working tree clean") == -1
 }
